bd15b93c97f55f09375e685e71b9b35a09188391
ankur003 <ankurbanssla@gmail.com>
2021-06-26 19:14:17 +0530

    added space

--- src/main/java/com/nidhi/cms/NidhiCmsApplication.java
+++ src/main/java/com/nidhi/cms/NidhiCmsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NidhiCmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NidhiCmsApplication.class, args);
+		SpringApplication.run(NidhiCmsApplication.class, args); 
 	}
 
 }
