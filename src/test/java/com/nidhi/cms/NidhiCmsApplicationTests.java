bd0eab70878051d5da0879e9a02b92cd0ed6cbed
Ankur Bansala <ankur.bansala@techblue.co.uk>
2021-06-26 19:04:36 +0530

    initial commit

---
+++ src/test/java/com/nidhi/cms/NidhiCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidhi.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NidhiCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
