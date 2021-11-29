81420db0f25fd2cca96bf05e4c09aa7c13c1befc
Ankur Bansala <ankur.bansala@techblue.co.uk>
2021-11-29 21:22:07 +0530

    d

--- src/main/java/com/nidhi/cms/controller/UserController.java
+++ src/main/java/com/nidhi/cms/controller/UserController.java
@@ -919,7 +919,6 @@ private static boolean getClientIpAddress(String ip2, HttpServletRequest request
 	
 	public List<String> getUserNameByMarchantId(String marchantId) {
 		return userWalletService.getUserNameByMarchantId(marchantId);
-		
 	}
 	
 }
